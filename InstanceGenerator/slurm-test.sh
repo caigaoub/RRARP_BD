57213a4b014db5172b928645ad74f44f53626381
caigaoub <caigao@srv-p22-12.cbls.ccr.buffalo.edu>
2020-02-26 15:12:50 -0500

    add slurm.sh

---
+++ InstanceGenerator/slurm-test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+##SBATCH --account=pi-josewalt
+#SBATCH --clusters=faculty
+#SBATCH --partition=isecc
+#SBATCH --qos=isecc
+##SBATCH --time=05:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+##SBATCH --mem=120000
+#SBATCH --job-name="ccr_test"
+#SBATCH --output="ccr_test.out"
+#SBATCH --error="ccr_test.err"
+##SBATCH --mail-user=josewalt@buffalo.edu
+##SBATCH --mail-type=ALL
+##SBATCH --requeue
+
+##echo "SLURM_JOB_ID="$SLURM_JOB_ID
+##echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+##echo "SLURM_NNODES"=$SLURM_NNODES
+##echo "SLURMTMPDIR="$SLURMTMPDIR
+##echo "SLURM_ARRAYID="$SLURM_ARRAYID
+##echo "SLURM_ARRAY_JOB_ID"=$SLURM_ARRAY_JOB_ID
+##echo "SLURM_ARRAY_TASK_ID"=$SLURM_ARRAY_TASK_ID
+##echo "working directory = "$SLURM_SUBMIT_DIR
+
+NPROCS=`srun --nodes=${SLURM_NNODES}$ bash -c 'hostname' |wc -l`
+echo "NPROCS="$NPROCS
+
+module load gurobi/9.0.0
+
+make
+
+./bin/main
+
+echo "All Done!"
+
