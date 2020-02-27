6662563973e784223b4a9a652c4386ec90a53a81
caigao <caigao@buffalo.edu>
2020-02-27 11:41:04 -0500

    add files

---
+++ BendersDecomp/ret/generate_config.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+
+
+"""  generate configuration files """
+
+# algo_idx = 4
+# nb_dstzn = 8
+
+
+
+
+instsize_min = 6
+instsize_max = 10
+idx_config = 1
+diff_levels = ['e','m','h']
+subidx_min = 1 
+subidx_max = 10
+for instsize in range(instsize_min, instsize_max+1):
+	for level in diff_levels:
+		for sidx in range(subidx_min, subidx_max+1):
+			file = open("./configs/config_" + str(idx_config), "w")
+			file.write("n_"+str(instsize) + '_' + level + '_' + str(sidx)+".txt")
+			file.close()
+			idx_config += 1
+
+	print("done with instance with", instsize, "targets")
